d7509c00e3c4222347cdd338286a64cf35c1d919
manzanillo <manza@outlook.de>
2019-06-04 16:49:23 +0200

    header bar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <md-toolbar class="md-toolbar-tools black">
+      <img alt="Vue logo" src="./assets/Header_Logo_White.svg">
+      <h3 class="md-title">SocialHubs</h3>
+    </md-toolbar>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.black {
+  background-color: #1e1e1e;
+  color: white;
+}
+
+.black > img {
+  width: 30%;
 }
 </style>
