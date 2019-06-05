51159ad8d3bc9ad26746bdc0b1b0e854e8f36ff0
manzanillo <manza@outlook.de>
2019-06-05 14:42:08 +0200

    new styling

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,14 +1,59 @@
 <template>
   <div id="app">
-    <md-card v-for="(entry,index) in schedule" :key="index">
+    <md-card
+      v-for="(entry,index) in schedule"
+      :key="index"
+      v-bind:class="[entry.skill ? 'customCol' : '']"
+    >
       <md-card-header>
-        <div class="md-title">{{entry.title}}</div>
+        <div class="md-title">
+          <div>
+            <md-icon class="eigene-size">access_time</md-icon>
+            {{entry.title}}
+          </div>
+        </div>
         <div class="md-subhead" v-if="entry.date">{{entry.date}}</div>
       </md-card-header>
 
-      <md-card-content>{{entry.content}}</md-card-content>
+      <md-card-content class="card-content-left">
+        <div v-if="entry.skill">
+          <div class="card-reservation">
+            <md-icon>location_on</md-icon>
+            {{entry.room}}
+          </div>
+          <div class="card-reservation">
+            <md-icon>person</md-icon>
+            {{entry.person1}}
+          </div>
 
-      <md-card-actions>
+          <div style="width:100%; 	border-top: 1px solid #8c8b8b; margin: 8px 0px;"></div>
+
+          <div class="card-reservation">
+            <md-icon>info</md-icon>
+            {{entry.skill}}
+          </div>
+        </div>
+
+        <div v-if="!entry.skill">
+          <div class="card-reservation">
+            <md-icon>location_on</md-icon>
+            {{entry.room}}
+          </div>
+          <div class="card-reservation">
+            <md-icon>group</md-icon>
+            {{entry.person1}} & {{entry.person2}}
+          </div>
+
+          <div style="width:100%; 	border-top: 1px solid #8c8b8b; margin: 8px 0px;"></div>
+
+          <div class="card-reservation">
+            <md-icon>location_off</md-icon>
+            Alternativer Raum: {{entry.room}}
+          </div>
+        </div>
+      </md-card-content>
+
+      <md-card-actions v-if="!entry.skill">
         <md-button class="md-raised md-primary">
           <router-link to="/lageplan">Lageplan</router-link>
         </md-button>
@@ -59,8 +104,9 @@ export default {
         title: "Lust auf Kaffee?",
         display: true,
         date: datestring,
-        content:
-          "Zucker Markberg ist gerade an der Kaffeemaschine, er kennt sich mit Facebook Ads aus!"
+        room: "Kaffeemaschine",
+        person1: "Zucker Markberg",
+        skill: "Zucker kennt sich mit Facebook Ads aus!"
       });
       /*      this.schedule.unshift({
         title: "20. - 24. Mai",
@@ -92,16 +138,14 @@ export default {
             title: obj,
             display: true,
             room: response.data.data[0].room,
-            content:
-              "Hallo Stefan, ich habe dich diese Woche mit " +
+            person1:
               response.data.data[0].firstname +
               " " +
-              response.data.data[0].lastname +
-              " und " +
+              response.data.data[0].lastname,
+            person2:
               response.data.data[1].firstname +
               " " +
-              response.data.data[1].lastname +
-              " zusammengebracht. Setz dich doch an Platz 4 in Raum 2."
+              response.data.data[1].lastname
           });
           //        this.schedule;
         })
@@ -162,5 +206,22 @@ export default {
 
 .md-card {
   margin: 1em;
+}
+
+.card-content-left {
+  text-align: left;
+  font-size: 110%;
+}
+
+.eigene-size {
+  margin: 4px 0px 8px 0px;
+  width: 24px;
+  min-width: 24px;
+  height: 24px;
+  font-size: 24px !important;
+}
+
+.customCol {
+  background-color: lightblue;
 }
 </style>
